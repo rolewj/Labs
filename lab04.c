0df7735625739f3f3fe684dfe9dbf6b0e511897c
rolewj <71218380+rolewj@users.noreply.github.com>
2020-10-02 21:35:48 +0700

    Create lab04.c

---
+++ lab04.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+
+int main(int argc, char** argv){
+	int n = 1;
+  int a = 0;
+	int b = 0;
+	int i = 0;
+	int k = 0;
+  scanf("%d", &n);
+  while(i < n){
+    a = a + 1;
+    k = a;
+    while(k > 0){
+      k = k / 10;
+      i = i + 1;
+    }
+  }
+  while(i >= n){
+  	b = a % 10;
+    a = a / 10;
+    i = i - 1;
+  }
+  printf("%d", b, a);
+  return 0;
+}
